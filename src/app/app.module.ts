e1d22147de4cd265cbff5ad860a046347ef35d05
Daniel Velazquez <danielmurs21@gmail.com>
2021-03-03 08:30:57 -0600

    maqueado terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BarMenuComponent } from './components/bar-menu/bar-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BarMenuComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/bar-menu/bar-menu.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bar-menu',
+  templateUrl: './bar-menu.component.html',
+  styles: [
+  ]
+})
+export class BarMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
